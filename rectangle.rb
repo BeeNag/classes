be582ad8fbd057e8da2051ae21161e5d4f025540
BeeNag <bnaglerose88@gmail.com>
2015-11-18 20:37:55 +0000

    Add .gitignore file

---
+++ rectangle.rb
@@ -0,0 +1,22 @@
+
+class Rectangle
+
+	def initialize(width,height)
+		puts "Hello from the constructor"
+		@width = width
+		@height = height
+	end
+
+	def set_width(w)
+		@width = w
+	end
+
+	def set_height(h)
+		@height = h
+	end
+
+	def area()
+	    @width * @height
+	end
+
+end
